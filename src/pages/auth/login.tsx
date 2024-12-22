2740e0e1e271ca0cf5fa8ec70200693d3afd8c6d
Kubsa Melkamu <kubsamlkm@gmail.com>
2024-12-22 16:44:40 +0300

    render login and register page

---
+++ src/pages/auth/login.tsx
@@ -0,0 +1,8 @@
+import Login from "@/componenets/login";
+
+
+const LoginPage: React.FC = () => {
+     return<Login/>
+};
+
+export default LoginPage;
---
+++ src/pages/auth/register.tsx
@@ -0,0 +1,8 @@
+import Register from "@/componenets/register";
+
+
+const RegisterPage: React.FC = () => {
+      return<Register/>
+};
+
+export default RegisterPage;
